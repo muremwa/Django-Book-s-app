09dc17f75d904e5337635a37e2b49c7f84e5cb8c
Muremwa <danmburu254@gmail.com>
2018-03-27 12:40:16 +0300

    Admin interface modified

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Book, Author
 
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 3
+
+
+
 class AuthorAdmin(admin.ModelAdmin):
     fields = ['author_id', 'name', 'picture', 'bio']
+
+    inlines = [BookInline]
+
 
 
 
@@ -10,8 +19,14 @@ class BookAdmin(admin.ModelAdmin):
     fieldsets = [
         ('book info', {'fields': ['book_id', 'title', 'number_of_pages', 'year_of_publication', 
         'author', 'publisher', 'genre']}),
-        ('others', {'fields': ['book_cover']})
+        ('others', {'fields': ['book_cover', 'is_favourite']})
     ]
+
+    list_display = ['book_id', 'title', 'votes']
+
+    list_filter = ['year_of_publication']
+
+    search_fields = ['title']
 
 
 admin.site.register(Book, BookAdmin)
